8d41d6db862fd29f040866f42396a2a418b21b3b
seungdangdang <yseungun.21@gmail.com>
2024-09-02 15:53:30 +0900

    chore: Header 레이아웃 수정

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -5,8 +5,7 @@ import useAxiosPost from "../hooks/useAxiosPost";
 
 const Header = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const {data, loading, postData} = useAxiosPost(
-      (`${API_URL}/login`));
+  const {data, loading, postData} = useAxiosPost((`${API_URL}/login`));
 
   useEffect(() => {
     const userid = localStorage.getItem('userid');
@@ -32,25 +31,33 @@ const Header = () => {
     setIsLoggedIn(false);
   };
 
-  return (
-      <header className="header">
-        <div className="header-left">QUICK DEAL</div>
-        <div className="header-center">
-          <input type="text" placeholder="Search..."
-                 className="search-input"/>
-        </div>
-        <div className="header-right">
-          {isLoggedIn ? (
-              <button onClick={handleLogout}>로그아웃</button>
-          ) : (
-              <button onClick={handleLogin} disabled={loading}>
-                {loading ? '로딩 중...' : '로그인'}
-              </button>
-          )}
-          <span>MY</span>
-        </div>
-      </header>
-  );
-};
+  return (<header className="header">
+    <div className="header-left">
+      <input
+          type="text"
+          placeholder="Search..."
+          className="search-input"
+      />
+    </div>
 
+    <div className="header-center">QUICK DEAL</div>
+
+    <div className="header-right">
+      {isLoggedIn ? (
+          <>
+            <button onClick={handleLogout}>로그아웃</button>
+            <button>마이페이지</button>
+          </>
+      ) : (
+          <>
+            <button onClick={handleLogin} disabled={loading}>
+              {loading ? '로딩 중...' : '로그인'}
+            </button>
+            <button>회원가입</button>
+          </>
+      )}
+    </div>
+  </header>)
+
+};
 export default Header;
