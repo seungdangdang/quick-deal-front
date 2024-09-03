14e3ec753a6ca47bc7c4f4e32eab04e97c197292
seungdangdang <yseungun.21@gmail.com>
2024-09-03 12:23:02 +0900

    feat: MainPage 인피니티스크롤 구현

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,18 +1,45 @@
 import {API_URL} from "../config";
 import ProductList from "../components/ProductList";
-import useAxiosGet from "../hooks/useAxiosGet";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const MainPage = () => {
-  const {data, loading} = useAxiosGet(`${API_URL}/products`);
-  const products = data && Array.isArray(data.products) ? data.products : [];
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [isFetching, setIsFetching] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+
+  // 초기 데이터 로딩
+  useEffect(() => {
+    const fetchInitialProducts = async () => {
+      try {
+        const {data} = await axios.get(`${API_URL}/products`);
+        setProducts(data.products);
+        if (data.products.length === 0) {
+          setHasMore(false);
+        }
+      } catch (error) {
+        console.error("초기 데이터 로드 오류:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchInitialProducts();
+  }, []);
 
   // 스크롤이 바닥에 닿았는지 인지
   useEffect(() => {
+    if (loading) {
+      return;
+    }
+
     const handleScroll = () => {
       if (window.innerHeight + window.scrollY
           >= document.documentElement.scrollHeight - 1) {
-        alert('스크롤이 바닥에 닿았음');
+        if (!isFetching && hasMore) {
+          fetchMoreProducts();
+        }
       }
     };
 
@@ -21,7 +48,30 @@ const MainPage = () => {
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, []);
+  }, [isFetching, hasMore, loading]);
+
+  // 추가 데이터 로딩
+  const fetchMoreProducts = async () => {
+    setIsFetching(true);
+    const lastProductId = products[products.length - 1]?.id;
+
+    setTimeout(async () => {
+      try {
+        const {data} = await axios.get(
+            `${API_URL}/products?lastId=${lastProductId}`);
+
+        if (data.products.length === 0) {
+          setHasMore(false);
+        } else {
+          setProducts((prevProducts) => [...prevProducts, ...data.products]);
+        }
+      } catch (error) {
+        console.error("추가 데이터 로드 오류:", error);
+      } finally {
+        setIsFetching(false);
+      }
+    }, 300);
+  };
 
   if (loading) {
     return <p> Loading ... </p>
@@ -30,6 +80,7 @@ const MainPage = () => {
   return (
       <>
         <ProductList products={products}/>
+        {isFetching && <p>Loading more products...</p>}
       </>
   );
 };
