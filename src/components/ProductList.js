15feb0f3fa2aab0ebd69d5313c5b08f6dd2b8571
seungdangdang <yseungun.21@gmail.com>
2024-09-03 11:27:01 +0900

    feat: MainPage 상품 리스트에 플레이스 홀더 추가 및 상품코드 출력

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import '../styles/ProductList.css';
+import {useNavigate} from "react-router-dom";
 
 const ProductList = ({products}) => {
+  const navigate = useNavigate();
+
   if (!Array.isArray(products) || products.length === 0) {
     return <p>No products available</p>;
   }
 
+  const handleProductClick = (productId) => {
+    navigate(`/products/${productId}`);
+  };
+
   return (
       <div className='product-grid'>
         {products.map((product) => (
-            <div key={product.id} className='product-card'>
+            <div
+                key={product.id}
+                className='product-card'
+                onClick={() => handleProductClick(product.id)}
+                style={{cursor: 'pointer'}}
+            >
+              <div className="product-list-image-placeholder"></div>
+              <p>상품코드 [{product.id}]</p>
               <h3>{product.name}</h3>
-              <p>{product.price}원</p>
+              <p>{product.price}</p>
               <p>{product.description}</p>
             </div>
         ))}
