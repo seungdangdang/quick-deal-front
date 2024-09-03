c935ae32fcf978a84019c15ff3adf9322b5f3abf
seungdangdang <yseungun.21@gmail.com>
2024-09-03 11:34:57 +0900

    feat: MainPage 스크롤이 바닥이 닿았을 때 인지하는 이벤트 리스너 생성

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,10 +1,27 @@
 import {API_URL} from "../config";
 import ProductList from "../components/ProductList";
 import useAxiosGet from "../hooks/useAxiosGet";
+import {useEffect} from "react";
 
 const MainPage = () => {
   const {data, loading} = useAxiosGet(`${API_URL}/products`);
   const products = data && Array.isArray(data.products) ? data.products : [];
+
+  // 스크롤이 바닥에 닿았는지 인지
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.innerHeight + window.scrollY
+          >= document.documentElement.scrollHeight - 1) {
+        alert('스크롤이 바닥에 닿았음');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   if (loading) {
     return <p> Loading ... </p>
