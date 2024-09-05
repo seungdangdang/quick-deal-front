0e4706d36342742742a115f040c840396c191e2e
seungdangdang <yseungun.21@gmail.com>
2024-09-05 20:44:59 +0900

    feat: Footer 관련 컴포넌트 생성 및 페이지를 Flex 컨테이너로 생성
    - 콘텐츠가 부족할 떄 페이지 화면 높이가 부족하여 푸터가 하단에서 위로 벗어나는 문제 해결을 위해 Flex 컨테이너로 변경

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import "./styles/App.css";
 import MainPage from "./pages/MainPage";
 import Header from "./layout/Header";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Footer from "./layout/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ProductDetailPage from "./pages/ProductDetailPage";
 import PaymentPage from "./pages/PaymentPage";
+import { Box } from "@mui/material";
 
 const App = () => {
   return (
-      <BrowserRouter>
-        <Header/>
-        <Routes>
-          <Route path={'/'} element={<MainPage/>}/>
-          <Route path="/products/:productId" element={<ProductDetailPage/>}/>
-          <Route path="/products/:productId/payment" element={<PaymentPage/>}/>
-        </Routes>
-      </BrowserRouter>
-  )
-}
+      <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            minHeight: '100vh',
+            fontFamily: 'Arial, sans-serif',
+            backgroundColor: '#f5f5f5',
+          }}
+      >
+        <BrowserRouter>
+          <Header />
+          <Box
+              sx={{
+                flex: 1,
+                maxWidth: '1500px',
+                margin: '0 auto',
+                padding: '0 20px',
+                width: '100%',
+                boxSizing: 'border-box'
+              }}
+          >
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/products/:productId" element={<ProductDetailPage />} />
+              <Route path="/products/:productId/payment" element={<PaymentPage />} />
+            </Routes>
+          </Box>
+          <Footer />
+        </BrowserRouter>
+      </Box>
+  );
+};
 
 export default App;
